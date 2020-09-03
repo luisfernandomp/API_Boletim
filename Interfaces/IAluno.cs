dc9240d3c36fbf5d954c7564deaf2021a6d3a0c6
luisfernandomp <53625024+luisfernandomp@users.noreply.github.com>
2020-09-02 21:22:04 -0300

    Projeto iniciado

---
+++ Interfaces/IAluno.cs
@@ -0,0 +1,17 @@
+﻿using API_Boletim.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Boletim.Interfaces
+{
+    interface IAluno
+    {
+        Aluno Cadastrar(Aluno a);
+        List<Aluno> LerTodos();
+        Aluno BuscarPorId(int id);
+        Aluno Alterar(Aluno a);
+        Aluno Excluir(Aluno a);
+    }
+}
---
+++ Repositories/AlunoRepository.cs
@@ -0,0 +1,38 @@
+﻿using API_Boletim.Domains;
+using API_Boletim.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Boletim.Repositories
+{
+    public class AlunoRepository : IAluno
+
+    {
+        public Aluno Alterar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Cadastrar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Excluir(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> LerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
